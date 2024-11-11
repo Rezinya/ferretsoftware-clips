c78a850f0b9a279e59d52ec09271991a291e200c
Rezi <58380975+Rezinya@users.noreply.github.com>
2024-11-11 15:57:09 -0800

    Updated README and packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    dynamicIO: true,
+  },
 };
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "clips-media-assets2.twitch.tv",
+        port: "",
+        pathname: "/**/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static-cdn.jtvnw.net",
+        port: "",
+        pathname: "/twitch-clips-thumbnails-prod/**",
+      },
+    ],
+  },
+}
 
 export default nextConfig;
